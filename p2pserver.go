f0829e946c7b511114dd0f4ce6117411f4549260
Danny Nielsen <danny.nlsn0@gmail.com>
2017-12-20 16:24:56 +0100

    Added draft of StartServer method.

--- p2pserver.go
+++ p2pserver.go
@@ -1,10 +1,39 @@
 package main
 
-// Struct to keep tab on a specific Blockchain instance.
-type version struct {
-	BestHeight  int
-	AddressFrom int
+import (
+	"log"
+	"net"
+)
+
+const protocol = "tcp"
+
+var fullNodeAddress string
+var knownNodes = []string{"172.19.0.2:8081", "172.19.0.3:8082", "172.19.0.4:8083", "172.19.0.5:8084"} //IP and ports of our peers/nodes
+
+//StartServer should connect to, and start a node
+func StartServer(peerAddress string) {
+	fullNodeAddress = peerAddress
+	_, port, _ := net.SplitHostPort(peerAddress)
+
+	listen, error := net.Listen(protocol, fullNodeAddress)
+	if error != nil {
+		panic(error)
+	}
+	defer listen.Close()
+
+	bc := NewBlockChain(port)
+
+	for {
+		connection, err := listen.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+		go handleConnection(connection, bc)
+
+	}
+
 }
 
-var nodeAddress string
-var knownNodes = []string{"172.19.0.2", "172.19.0.3", "172.19.0.4", "172.19.0.5"}
+func handleConnection(conn net.Conn, bc *Blockchain) {
+
+}
